cc1427b37ba55220260f99164eb3e0ffac07f547
Bogdan Lishchynskiy <lishchynskiy91@gmail.com>
2018-01-22 19:41:42 +0200

    onload event add

--- js/index.js
+++ js/index.js
@@ -1,6 +1,4 @@
 var el = document.querySelectorAll('.maincheck');
-
-// console.log(el);
 for (var i = 0; i < el.length; i++) {
     let curEl = el[i];
     el[i].addEventListener('click', function () {
@@ -12,12 +10,7 @@ for (var i = 0; i < el.length; i++) {
 
 function hideFunc(el) {
     var parentDiv = el.closest(".components");
-    //console.log(parentDiv);
-
     var hiddenEl = parentDiv.getElementsByClassName('hide');
-    // console.log(hiddenEl);
-    // var attributeStage = el.hasAttribute("checked");
-    // console.log(attributeStage);
     for (var i = 0; i < hiddenEl.length; i++) {
         if (el.checked === true) {
             hiddenEl[i].style.visibility = "";
@@ -38,66 +31,6 @@ function hideElems(){
 
 
 
-
-// if (el.checked === true ) {
-//     hiddenEl.visibility = "hidden";
-//  }
-// else {
-//     hiddenEl.style.visibility = "";    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// console.log(hiddenEl);
-
-// for (var i = 0; i < hiddenEl.length; i++) {
-
-// console.log(hiddenEl[i]);
-// // }
-
-// // else  {
-// //     console.log('rd');
-// // }
-
-
-//  function hideFunc() {
-//     console.log(gg);
-//  }
-
-// var getClosest = function () {
-//     for ( ; el && el !== document; el = el.parentNode ) {
-//         if ( el.matches( '.components' ) ) return el;
-//         console.log(el);
-//     }
-//     return null;
-// };
-
-
-
-
-
-
-
-// if (event.target.checked === true) {
-//     var inpt = document.getElementById('hid').hidden = true;
-//   } else  {
-//     inpt = document.getElementById('hid').hidden = false;
-//   }
 
 
 
